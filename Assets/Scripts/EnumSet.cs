55d5554f8e1d76c8c1e23cdee976db43b11a1883
YoonwooLee <as04107@gmail.com>
2022-07-09 15:07:22 +0900

    [#Feature] EnumSet추가

---
+++ Assets/Scripts/EnumSet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumSet
+{
+    public enum BehaviourState
+    {
+
+    }
+
+    public enum ActingPowerState
+    {
+
+    }
+}
